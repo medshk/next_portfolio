0824c4965a5b2d30f256780df4fb31aeee2b0045
medshk <47352636+medshk@users.noreply.github.com>
2022-03-07 22:36:44 +0100

    fixing logo size on all  screens

--- components/layout/sideBar.js
+++ components/layout/sideBar.js
@@ -9,8 +9,8 @@ const SideBar = ({ toggleMenuBar }) => {
     return (
         <div className={`${toggleMenuBar ? "scale-100" : "scale-0"} bg-[#181818] w-full fixed top-0 left-0 lg:w-36 min-h-full lg:scale-100 z-[5] transition ease-out duration-200 `}>   
            <Link href="/#home">
-                <a className="logo h-fit bg-black text-center w-full">
-                        <Image src={logo} className="w-full cursor-pointer" layout='responsive'/>
+                <a className="logo inline-block bg-black text-center w-full">
+                        <Image src={logo} className="w-full cursor-pointer"/>
                 </a>
            </Link>
             <nav className='flex flex-col items-center  text-lg text-gray-400 group'>
